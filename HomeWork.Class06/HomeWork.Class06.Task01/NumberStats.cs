9a01ff5146d130b12dd09a9b43508060fa5da216
Mario <mario.stojkovski15@gmail.com>
2022-05-03 11:33:34 +0200

    New Update!

---
+++ HomeWork.Class06/HomeWork.Class06.Task01/NumberStats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork.Class06.Task01
+{
+    public class NumberStats
+    {
+        public double Number { get; set; }
+
+        public bool isEven { get; set; }
+        public bool isNegative { get; set; }
+        public bool isDecimal { get; set; }
+
+        public void PrintInfo()
+        {
+            string str = string.Format(@"Stats for number: {0} {1} {2}",
+                Number,
+                isNegative ? "Negarive" : "Positive",
+                isDecimal ? "Decimal" : "Intager",
+                isEven ? "Even" : "Odd"
+                );
+            Console.WriteLine(str);
+        }
+
+
+    }
+}
---
+++ HomeWork.Class06/HomeWork.Class06.Task01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork.Class06.Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[] { 3, 52, 11.6, -6.3, -60,  3123, 0.5, 43235.2 };
+
+            foreach (double number in numbers)
+            {
+                NumberStats numberStats = NumberStats(number);
+                numberStats.PrintInfo();
+            }
+
+            static NumberStats NumberStats(double number)
+            {
+                NumberStats stats = new NumberStats()
+                {
+                    Number = number
+                };
+                stats.isEven = number % 2 == 0;
+                stats.isDecimal = number % 1 != 0;
+                stats.isNegative = number < 0;
+                return stats;
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
